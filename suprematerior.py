8184d31a77c09928d5ffef924ed5479a83c846d8
Evgeny Vetlugin <vetlugin@spherasveta.ru>
2019-04-16 10:11:59 +0300

    Шаг 3-13

--- suprematerior.py
+++ suprematerior.py
@@ -3,6 +3,15 @@ from instabot import Bot
 
 bot = Bot()
 bot.login(username="Suprematerior", password="Vspleskroot44")
-user_id = bot.get_user_id_from_username("lego")
+user_id = bot.get_user_id_from_username("beautybar.rus")
+#user_id = bot.get_user_id_from_username("spherasveta")
 user_info = bot.get_user_info(user_id)
-print(user_info['biography'])
+#print(user_info['biography'])
+
+id_media = bot.get_media_id_from_link("https://www.instagram.com/p/BtON034lPhu/")
+#id_media = bot.get_media_id_from_link("https://www.instagram.com/p/BvMQ7rSAhK0/")
+print(id_media)
+
+all_comments = bot.get_media_comments_all(id_media)
+for item in range(len(all_comments)):
+    print(all_comments[item]['text'])
