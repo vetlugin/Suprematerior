514abdb8d695789c02adf1c35ab95905baeb9aa2
Evgeny Vetlugin <vetlugin@spherasveta.ru>
2019-04-17 11:04:20 +0300

    Шаг 5-13

--- suprematerior.py
+++ suprematerior.py
@@ -1,17 +1,40 @@
 from instabot import Bot
+import re
 
 
-bot = Bot()
-bot.login(username="Suprematerior", password="Vspleskroot44")
-user_id = bot.get_user_id_from_username("beautybar.rus")
-#user_id = bot.get_user_id_from_username("spherasveta")
-user_info = bot.get_user_info(user_id)
-#print(user_info['biography'])
+def get_users_from_comment(comment):
+        result = re.findall(r'@([A-Za-z0-9_]+)', comment)
+        return result
 
-id_media = bot.get_media_id_from_link("https://www.instagram.com/p/BtON034lPhu/")
-#id_media = bot.get_media_id_from_link("https://www.instagram.com/p/BvMQ7rSAhK0/")
-print(id_media)
 
-all_comments = bot.get_media_comments_all(id_media)
-for item in range(len(all_comments)):
-    print(all_comments[item]['text'])
+def is_user_exist (username):
+    bot = Bot()
+    bot.login(username="Suprematerior", password="Vspleskroot44")
+
+    try:
+        user_id = bot.get_user_id_from_username(username)
+    except JSONDecodeError:
+        return
+
+    return user_id
+
+def main():
+    bot = Bot()
+    bot.login(username="Suprematerior", password="Vspleskroot44")
+    user_id = bot.get_user_id_from_username("beautybar.rus")
+    #user_id = bot.get_user_id_from_username("spherasveta")
+    user_info = bot.get_user_info(user_id)
+    #print(user_info['biography'])
+
+    id_media = bot.get_media_id_from_link("https://www.instagram.com/p/BtON034lPhu/")
+    #id_media = bot.get_media_id_from_link("https://www.instagram.com/p/BvMQ7rSAhK0/")
+
+    #all_comments = bot.get_media_comments_all(id_media)
+
+    print(is_user_exist('lkspherasveta'))
+#    for item in range(len(all_comments)):
+#        print(get_users_from_comment(all_comments[item]['text']))
+
+
+if __name__ == '__main__':
+    main()
