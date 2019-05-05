1a746c810806cff6e5027409e53a7367f87bac38
Evgeny Vetlugin <vetlugin@spherasveta.ru>
2019-05-05 17:34:14 +0300

    Update suprematerior.py

--- suprematerior.py
+++ suprematerior.py
@@ -4,12 +4,12 @@ import pprint
 
 
 def get_users_from_comment(comment):
-        result = re.findall(r'@([A-Za-z0-9_]+)', comment)
+    result = re.findall(r'@([A-Za-z0-9_]+)', comment)
 
-        if not result:
-            return None
-
-        return result
+#        if not result:
+#            return None
+#
+    return result
 
 
 def is_user_exist (username):
@@ -27,17 +27,12 @@ def main():
     bot = Bot()
     bot.login(username="Suprematerior", password="Vspleskroot44")
     user_id = bot.get_user_id_from_username("beautybar.rus")
-    #user_id = bot.get_user_id_from_username("spherasveta")
-    #user_info = bot.get_user_info(user_id)
-    #pprint.pprint(user_info)
 
-
-    #id_media = bot.get_media_id_from_link("https://www.instagram.com/p/Bwu6M2pgRuZ/")
     id_media = bot.get_media_id_from_link("https://www.instagram.com/p/BtON034lPhu/")
+    #id_media = bot.get_media_id_from_link("https://www.instagram.com/p/Bwu6M2pgRuZ/")
     #id_media = bot.get_media_id_from_link("https://www.instagram.com/p/BvMQ7rSAhK0/")
 
     all_comments = bot.get_media_comments_all(id_media)
-    #pprint.pprint(bot.get_media_comments_all(id_media))
 
     list_of_users = []
     list_of_likers = bot.get_media_likers(id_media)
@@ -49,10 +44,15 @@ def main():
         and list_of_followers.count(str(comment['user_id'])):
             list_of_users.append((comment['user_id'], comment['user']['username']))
 
-    print(f'list_of_user = {len(list_of_users)}')
-    print(f'list_of_likers = {len(list_of_likers)}')
-    print(f'list_of_followers = {len(list_of_followers)}')
-    print(f'action_users = {len(list(set(list_of_users)))}')
+    #print(f'list_of_user = {len(list_of_users)}')
+    #print(f'list_of_likers = {len(list_of_likers)}')
+    #print(f'list_of_followers = {len(list_of_followers)}')
+    #print(f'action_users = {len(list(set(list_of_users)))}')
+
+    action_users = list(set(list_of_users))
+
+    for user in action_users:
+        print (user[1])
     #pprint.pprint()
 
 
