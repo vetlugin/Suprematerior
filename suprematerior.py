430973d27ceb9203dc1036e44197df8a9c150a68
Evgeny Vetlugin <vetlugin@spherasveta.ru>
2019-05-05 17:21:18 +0300

    Выводится количества разных категорий пользователей

--- suprematerior.py
+++ suprematerior.py
@@ -1,9 +1,14 @@
 from instabot import Bot
 import re
+import pprint
 
 
 def get_users_from_comment(comment):
         result = re.findall(r'@([A-Za-z0-9_]+)', comment)
+
+        if not result:
+            return None
+
         return result
 
 
@@ -14,26 +19,41 @@ def is_user_exist (username):
     try:
         user_id = bot.get_user_id_from_username(username)
     except JSONDecodeError:
-        return
+        return False
+    return True
 
-    return user_id
 
 def main():
     bot = Bot()
     bot.login(username="Suprematerior", password="Vspleskroot44")
     user_id = bot.get_user_id_from_username("beautybar.rus")
     #user_id = bot.get_user_id_from_username("spherasveta")
-    user_info = bot.get_user_info(user_id)
-    #print(user_info['biography'])
+    #user_info = bot.get_user_info(user_id)
+    #pprint.pprint(user_info)
 
+
+    #id_media = bot.get_media_id_from_link("https://www.instagram.com/p/Bwu6M2pgRuZ/")
     id_media = bot.get_media_id_from_link("https://www.instagram.com/p/BtON034lPhu/")
     #id_media = bot.get_media_id_from_link("https://www.instagram.com/p/BvMQ7rSAhK0/")
 
-    #all_comments = bot.get_media_comments_all(id_media)
+    all_comments = bot.get_media_comments_all(id_media)
+    #pprint.pprint(bot.get_media_comments_all(id_media))
 
-    print(is_user_exist('lkspherasveta'))
-#    for item in range(len(all_comments)):
-#        print(get_users_from_comment(all_comments[item]['text']))
+    list_of_users = []
+    list_of_likers = bot.get_media_likers(id_media)
+    list_of_followers = bot.get_user_followers(user_id)
+
+    for comment in all_comments:
+        if get_users_from_comment(comment['text']) is not None \
+        and list_of_likers.count(str(comment['user_id'])) \
+        and list_of_followers.count(str(comment['user_id'])):
+            list_of_users.append((comment['user_id'], comment['user']['username']))
+
+    print(f'list_of_user = {len(list_of_users)}')
+    print(f'list_of_likers = {len(list_of_likers)}')
+    print(f'list_of_followers = {len(list_of_followers)}')
+    print(f'action_users = {len(list(set(list_of_users)))}')
+    #pprint.pprint()
 
 
 if __name__ == '__main__':
